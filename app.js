a0d44dc9264f9a7587955d410faa957452125153
laerciosb <laerciosbezerra@gmail.com>
2016-02-22 10:03:44 -0300

    cors adding.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var fogo_machines = require('./routes/fogo_machines');
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', routes);
 app.use('/fogo_machines', fogo_machines);
 app.use('/fogo_controllers', fogo_controllers);
